16d34aeed6e2a7ea4bcbc5e48eb0dfc49166f173
Wojciech M <wojtekm@myprograme.com>
2022-04-06 12:08:31 +0200

    Dodaj pliki projektów.

---
+++ fiszki/Form1.cs
@@ -0,0 +1,17 @@
+namespace fiszki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    // TODO: stworzyæ widok dla dodawania nowych fiszek
+    // HACk dodanie uk³adu z kartami
+    // TODO: stworzyæ widok dla dodawania nowych zestawu
+    // TODO po³¹czenie siê z baz¹ danych
+    // TODO: stworzyæ widok dla wyœwietlania listy fiszek
+    // TODO: stworzyæ widok dla wyœwietlanie fiszek dla konkretnego zetawu
+}
